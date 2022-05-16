c8fd8f7577723fcb9d717caf8fe3a3e18b3f9dc4
ZuhoorHadi <zuhoorhadi@gmail.com>
2022-05-15 22:41:06 -0400

    1st commit added

---
+++ src/com/company/GitHubPractice1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class GitHubPractice1 {
+    public static void main(String[] args) {
+
+        String car="red";
+        String door="four";
+        double engine=1000;
+
+        System.out.println(car);
+
+        int number=100;
+        int number2=1;
+
+        for (int i=1; i<100; i++)
+
+        System.out.println(i);
+
+    }
+}
